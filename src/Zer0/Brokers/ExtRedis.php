9be5032179226739abef8259473cbd3f596e0809
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2020-06-30 15:31:59 +0300

    ExtRedis: read_timeout

--- src/Zer0/Brokers/ExtRedis.php
+++ src/Zer0/Brokers/ExtRedis.php
@@ -32,7 +32,9 @@ class ExtRedis extends Base
         if ($type === 'standalone') {
             $redis = new \Redis();
             $redis->connect($config->server ?? '127.0.0.1', $config->port ?? 6379, $config->timeout ?? 0);
-            $redis->setOption(\Redis::OPT_READ_TIMEOUT, $config->read_timeout ?? $config->timeout ?? 0);
+            if (($config->read_timeout ?? null) !== null) {
+                $redis->setOption(\Redis::OPT_READ_TIMEOUT, $config->read_timeout ?? $config->timeout ?? 0);
+            }
             if ($config->database ?? false) {
                 $redis->select($config->database);
             }
